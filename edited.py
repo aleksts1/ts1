fbdafba8777318dce5793b431f2149dbcae3ece8
aleksts1 <45367431+aleksts1@users.noreply.github.com>
2018-11-26 19:04:08 +0100

    Update edited.py

--- edited.py
+++ edited.py
@@ -6,7 +6,7 @@ matplotlib.use("Agg")
 import pandas as pd
 import numpy as np
 from glob import glob
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -24,38 +24,131 @@ from sklearn.model_selection import train_test_split
 #%%
 imgHeight, imgWidth = 96, 96
 trainDir = "D:/TS1/Cancer/train/"
-evaluationDir = "D:/TS1/Cancer/test/"
+testDir = "D:/TS1/Cancer/test/"
 BATCHSIZE = 32
 EPOCHS = 1
 
 # open csv
 train_df = pd.read_csv("D:/TS1/Cancer/train_labels.csv")
+test_df = pd.read_csv("D:/TS1/Cancer/sample_submission.csv")
 print('training:')
 print(train_df.head())
 
 id_label_map = {k:v for k,v in zip(train_df.id.values, train_df.label.values)}
-print(id_label_map)
+#print(id_label_map)
 #%% 
 # load files
-labeled_files = glob('./train/*.tif')
-test_files = glob('./test/*.tif')
+labeled_files = glob(trainDir +'*.tif')
+test_files = glob(testDir + '*.tif')
 
 print("Train files #: {}".format(len(labeled_files)))
 print("Evaluation files #: {}".format(len(test_files)))
 
+      
+      
 #%%
-# data generator
+# initializing architecture
+print("[INFO] creating architecture..")
+model = Sequential()
+model.add(Conv2D(16, (3, 3), input_shape=(imgWidth, imgHeight, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.1))
+
+model.add(Conv2D(16, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.1))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+opt = Adam(lr=0.001)
+
+model.compile(loss='binary_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+      
+#%%
+# training data generator
 
 datagen = ImageDataGenerator(
     rescale=1./255)
 
 print("[INFO] train generator..")
 train_generator = datagen.flow_from_dataframe(dataframe=train_df,
-                                                directory="./train/",
+                                                directory=trainDir,
                                                 x_col='id',
                                                 y_col='label',
                                                 has_ext=False,
                                                 class_mode='binary',
                                                 target_size=(imgHeight,imgWidth),
                                                 batch_size=BATCHSIZE)
-print((len(train_generator[3])))                                               
+                                             
+#%%
+# testing data generator
+#testImages = testDir.
+print("[INFO] test generator")
+test_generator = datagen.flow_from_dataframe(dataframe=test_df,
+                                                directory=testDir,
+                                                x_col='id',
+                                                y_col=None,
+                                                has_ext=False,
+                                                class_mode=None,
+                                                target_size=(imgHeight,imgWidth),
+                                                batch_size=BATCHSIZE)
+print((test_generator[0].shape))
+#%%
+# compiling model
+H = model.fit_generator(train_generator, epochs=EPOCHS,
+                        steps_per_epoch=train_generator.n//BATCHSIZE)
+
+#%%
+# debug 
+print(len(test_generator))
+
+#%%
+# predict
+pred = model.predict_on_batch(train_generator[0])
+print(pred)
+
+#%%
+# predict generator
+pred_gen = model.evaluate_generator(train_generator,steps=len(train_generator))
+print(pred_gen)
+#%%
+# saving model
+print("[INFO] saving model...")
+
+model.save('model.model')        
+model.save_weights('first_try.h5')
+
+#%%
+# prediction
+print("[INFO] making predictions")
+pred_gen = model.predict_generator(test_generator,steps=len(test_generator))
+
+print("----------------------------------------------------------")
+print("Predictions test: ")
+print(pred_gen)`
+
+print("Predictions test over.. ")
+print("----------------------------------------------------------")
+
+# evaluation test
+print("[INFO] making evaluations")
+eval_test = model.evaluate(testX, trainLabels, batch_size=BATCHSIZE)
+print("----------------------------------------------------------")
+print("Evaluation test: ")
+print(pred_test)
+
+print("Evaluation test over.. ")
+print("----------------------------------------------------------")
